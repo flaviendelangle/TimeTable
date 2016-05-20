93284803410c6beae5a100d150212b9ca2584e98
flaviendelangle <flaviendelangle@gmail.com>
2016-05-20 16:30:12 +0200

    Restore controller

--- edu.telecom.lille.projet.info.etudiantTimeTable/src/timeTableController/TimeTableController.java
+++ edu.telecom.lille.projet.info.etudiantTimeTable/src/timeTableController/TimeTableController.java
@@ -1,58 +1,135 @@
-/*******************************************************************************
- * 2016, All rights reserved.
- *******************************************************************************/
 package timeTableController;
 
+import java.util.Date;
+import java.util.Hashtable;
+
 import timeTableModel.TimeTableDB;
-
-// Start of user code (user defined imports)
-
-// End of user code
-
 /**
- * Description of TimeTableController.
+ * Cette classe est le contrôleur d'emplois du temps que vous devez implémenter. 
+ * Elle contient un attribut correspondant à la base de données d'emplois du temps que vous allez créer.
+ * Elle contient toutes les fonctions de l'interface ITimeTableController que vous devez implémenter.
  * 
- * @author delangle
+ * @author Jose Mennesson (Mettre à jour)
+ * @version 04/2016 (Mettre à jour)
+ * 
  */
-public class TimeTableController implements ITimeTableController {
-	/**
-	 * Description of the property timeTableDB.
-	 */
-	public TimeTableDB timeTableDB = null;
 
-	// Start of user code (user defined attributes for TimeTableController)
+//TODO Classe à modifier
 
-	// End of user code
+public class TimeTableController implements ITimeTableController{
 
 	/**
-	 * The constructor.
+	 * Contient une instance de base de données d'emplois du temps
+	 * 
 	 */
-	public TimeTableController() {
-		// Start of user code constructor for TimeTableController)
-		super();
-		// End of user code
+	TimeTableDB tTDB;
+	/**
+	 * Constructeur de controleur d'emplois du temps créant la base de données d'emplois du temps
+	 * 
+	 * @param tTfile
+	 * 		Fichier XML contenant la base de données d'emplois du temps
+	 */
+	public TimeTableController(String tTfile) {
+		TimeTableDB tTDB=new TimeTableDB(tTfile);
+		this.tTDB=tTDB;
 	}
 
-	// Start of user code (user defined methods for TimeTableController)
-
-	// End of user code
-	/**
-	 * Returns timeTableDB.
-	 * @return timeTableDB 
-	 */
-	public TimeTableDB getTimeTableDB() {
-		return this.timeTableDB;
+	@Override
+	public String getTeacherLogin(int timeTableId, int bookId) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	/**
-	 * Sets a value to attribute timeTableDB. 
-	 * @param newTimeTableDB 
-	 */
-	public void setTimeTableDB(TimeTableDB newTimeTableDB) {
-		if (this.timeTableDB != null) {
-			this.timeTableDB.set(null);
-		}
-		this.timeTableDB.set(this);
+	@Override
+	public String[] roomsIdToString() {
+		// TODO Auto-generated method stub
+		return null;
 	}
+
+	@Override
+	public String[] roomsToString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] timeTablesIDToString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] booksIdToString(int timeTableId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addRoom(int roomId, int capacity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeRoom(int roomId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getRoom(int timeTableId, int bookId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean addTimeTable(int timeTableId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeTimeTable(int timeTableId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addBooking(int timeTableId, int bookingId, String login, Date dateBegin, Date dateEnd, int roomId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void getBookingsDate(int timeTableId, Hashtable<Integer, Date> dateBegin, Hashtable<Integer, Date> dateEnd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean removeBook(int timeTableId, int bookId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getBookingsMaxId(int timeTableId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean saveDB() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean loadDB() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
 
 }
